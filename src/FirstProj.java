ff45fb65404ec88d3bcf4fe5aa477eb5b212a772
Rodrigo Doná Tavares <rdtavares2009@gmail.com>
2021-12-11 13:59:14 -0300

    Atualização com comentários

--- src/FirstProj.java
+++ src/FirstProj.java
@@ -7,23 +7,24 @@ public class FirstProj {
     public static final int MAX_FALTAS = 15; // máximo de faltas
 
     public static void main(String[] args) {
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            try {
-                System.out.print("Nota do aluno: ");
-                float nota = scanner.nextFloat();
-                System.out.print("Quantidade de faltas: ");
-                int faltas = scanner.nextInt();
+        while (true) { // caso a entrada de dados for inválida, o while executará novamente o programa.
+            Scanner scanner = new Scanner(System.in); //iniciando scanner
+            try { // tente fazer isso
 
-                if (nota >= MEDIA && faltas <= MAX_FALTAS) {
-                    System.out.println("Aprovado");
+                System.out.print("Nota do aluno: ");
+                float nota = scanner.nextFloat(); // linha de entrada de float
+                System.out.print("Quantidade de faltas: ");
+                int faltas = scanner.nextInt(); // linha de entrada de int
+
+                if (nota >= MEDIA && faltas <= MAX_FALTAS) { // se (nota for maior ou igual a media E as faltas menores ou iguais o maximo de faltas)
+                    System.out.println("->Aprovado");
                 }
-                else {
-                    System.out.println("Reprovado");
+                else { // senão
+                    System.out.println("->Reprovado");
                 }
-                break;
-            } catch (InputMismatchException ex) {
-                System.out.println("Informe apenas numeros, tente novamente.");
+                break; // para o laço
+            } catch (InputMismatchException ex) { // pega o erro; InputMismatchException é o erro
+                System.out.println("Informe apenas numeros, tente novamente."); // mensagem de erro
                 // ex.printStackTrace();
                 System.out.println("============================");
             }
